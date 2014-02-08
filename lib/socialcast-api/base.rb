745163880424761125bafd468fe51485de24cf79
ganeshran <ganesh.ranganathan.85@gmail.com>
2014-02-08 09:58:32 +0530

    Matching end is not there

--- lib/socialcast-api/base.rb
+++ lib/socialcast-api/base.rb
@@ -1,153 +1,154 @@
 module SocialcastApi
-  class Base < ActiveResource::Base
-    self.site = SocialcastApi.configuration.site
-    if(SocialcastApi.configuration.oauthtoken.to_s == '')
-      self.user = SocialcastApi.configuration.user
-      self.password = SocialcastApi.configuration.password
-    else
-     self.headers['authorization'] = 'Bearer ' + SocialcastApi.configuration.oauthtoken.to_s
-  end
+	class Base < ActiveResource::Base
+		self.site = SocialcastApi.configuration.site
+		if(SocialcastApi.configuration.oauthtoken.to_s == '')
+			self.user = SocialcastApi.configuration.user
+			self.password = SocialcastApi.configuration.password
+		else
+			self.headers['authorization'] = 'Bearer ' + SocialcastApi.configuration.oauthtoken.to_s
+		end
 
-  class Message < Base
+		class Message < Base
 
-    class Like < Base
-      self.site = Message.site.to_s + "messages/:message_id/"
-    end
+			class Like < Base
+				self.site = Message.site.to_s + "messages/:message_id/"
+			end
 
-    class Flag < Base
-      self.site = Message.site.to_s + "messages/:message_id/"
-    end
+			class Flag < Base
+				self.site = Message.site.to_s + "messages/:message_id/"
+			end
 
-    def self.search(args = {})
-      get(:search, args).map {|message| Message.new(message)}
-    end
+			def self.search(args = {})
+				get(:search, args).map {|message| Message.new(message)}
+			end
 
-    def self.search_all_pages(args = {})
-      results = Array.new
-      page = 1
-      per_page = 500    
-  
-      begin
-        messages = get(:search, :page => page, :per_page => per_page, :q => args[:q]).map {|message| Message.new(message)}
-        page += 1
-        results += messages
-      end until messages.count < per_page
+			def self.search_all_pages(args = {})
+				results = Array.new
+				page = 1
+				per_page = 500    
 
-      return results
-    end
+				begin
+					messages = get(:search, :page => page, :per_page => per_page, :q => args[:q]).map {|message| Message.new(message)}
+					page += 1
+					results += messages
+				end until messages.count < per_page
 
-    def like
-      Message.post(id.to_s + '/likes')
-    end
+				return results
+			end
 
-    def unlike
-      likedbyme = likes.select {|alike| alike.unlikable}.first
-      if likedbyme
-        likedbyme.prefix_options = {:message_id => self.id}
-        likedbyme.destroy 
-      end
-    end
+			def like
+				Message.post(id.to_s + '/likes')
+			end
 
-    def flag
-      Message.post(id.to_s + '/flags')
-    end
+			def unlike
+				likedbyme = likes.select {|alike| alike.unlikable}.first
+				if likedbyme
+					likedbyme.prefix_options = {:message_id => self.id}
+					likedbyme.destroy 
+				end
+			end
 
-    def unflag
-      Message.delete(id.to_s + '/flags/' + attributes[:flag].id)
-    end
+			def flag
+				Message.post(id.to_s + '/flags')
+			end
 
-  end
+			def unflag
+				Message.delete(id.to_s + '/flags/' + attributes[:flag].id)
+			end
 
-  class Comment < Base
+		end
 
-    self.site = Message.site.to_s + "messages/:message_id/"
+		class Comment < Base
 
-    class Like < Base
-      self.site = Comment.site.to_s + "comments/:comment_id/"
-    end
+			self.site = Message.site.to_s + "messages/:message_id/"
 
-    def like
-      Comment.post(id.to_s + '/likes')
-    end
+			class Like < Base
+				self.site = Comment.site.to_s + "comments/:comment_id/"
+			end
 
-    def unlike
-      likedbyme = likes.select {|alike| alike.unlikable}.first
-      if likedbyme
-        likedbyme.prefix_options = {:message_id => message_id, :comment_id => self.id}
-        likedbyme.destroy 
-      end
-    end
+			def like
+				Comment.post(id.to_s + '/likes')
+			end
 
-  end
+			def unlike
+				likedbyme = likes.select {|alike| alike.unlikable}.first
+				if likedbyme
+					likedbyme.prefix_options = {:message_id => message_id, :comment_id => self.id}
+					likedbyme.destroy 
+				end
+			end
 
-  class User < Base
+		end
 
-    def self.search(args = {})
-      get(:search, args).map {|user| User.new(user)}
-    end
+		class User < Base
 
-    def messages
-      get(:messages).map {|message| Message.new(message)}
-    end
+			def self.search(args = {})
+				get(:search, args).map {|user| User.new(user)}
+			end
 
-    def followers(args = {})
-      get(:followers, args).map {|follower| User.new(follower)}
-    end
+			def messages
+				get(:messages).map {|message| Message.new(message)}
+			end
 
-    def follow
-      User.post(id.to_s + '/followers')
-    end
+			def followers(args = {})
+				get(:followers, args).map {|follower| User.new(follower)}
+			end
 
-    def unfollow
-      delete('followers/' + contact_id.to_s)  
-    end
+			def follow
+				User.post(id.to_s + '/followers')
+			end
 
-    def following(args = {})
-      get(:following, args).map {|user| User.new(user)}
-    end
+			def unfollow
+				delete('followers/' + contact_id.to_s)  
+			end
 
-    def self.all_pages
-      results = Array.new
-      page = 1
-      per_page = 500    
-  
-      begin
-        users = all(:params => {:page => page, :per_page => per_page})
-        page += 1
-        results += users
-      end until users.count < per_page
+			def following(args = {})
+				get(:following, args).map {|user| User.new(user)}
+			end
 
-      return results
-    end
+			def self.all_pages
+				results = Array.new
+				page = 1
+				per_page = 500    
 
-  end
+				begin
+					users = all(:params => {:page => page, :per_page => per_page})
+					page += 1
+					results += users
+				end until users.count < per_page
 
-  class Stream < Base
+				return results
+			end
 
-    def messages(args = {})
-      get(:messages, args).map {|message| Message.new(message)}
-    end
+		end
 
-  end
+		class Stream < Base
 
-  class Group < Base
+			def messages(args = {})
+				get(:messages, args).map {|message| Message.new(message)}
+			end
 
-    def members(args = {})
-      get(:members, args).map {|member| User.new(member)}
-    end
+		end
 
-  end
+		class Group < Base
 
-  class GroupMembership < Base
-  end
+			def members(args = {})
+				get(:members, args).map {|member| User.new(member)}
+			end
 
-  class Category < Base
-  end
+		end
 
-  class ContentFilter < Base
-  end
+		class GroupMembership < Base
+		end
 
-  # This class is not working or tested yet, just a frame!
-  class Attachment < Base
-  end
+		class Category < Base
+		end
+
+		class ContentFilter < Base
+		end
+
+		# This class is not working or tested yet, just a frame!
+		class Attachment < Base
+		end
+	end
 end
